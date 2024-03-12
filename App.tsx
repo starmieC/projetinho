d6003e45fa863e30b21540e89b33682a4e8f38cc
starmieC <oliveiracamille829@gmail.com>
2024-03-11 22:03:29 -0300

    atualização

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Ola mundothth</Text>
+      <Text style={styles.field}>Ponha teu nome abaixo:</Text>
+      <TextInput style={styles.caixaTexto}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'gray',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  field:{
+    color: '#fff',
+    fontSize: 20,
+  },
+
+  caixaTexto:{
+    backgroundColor: 'black',
+    width: 300,
+    borderRadius: 10,
+    padding: 15,
+    marginTop: 20
+  },
+
 });
